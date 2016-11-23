328be3755a12a24e8536c522dd8bfcf9dbbb822e
shereen messi <shereenmessi@hotmail.com>
2016-11-23 20:51:27 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ DBNAME = "tvshows"
 
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
+#Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
 
 connection_details = YAML::load(File.open('config/database.yml'))
 
